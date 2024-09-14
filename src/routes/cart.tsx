0e367b3c5fa20c43b1f4bb28c06ac51c27f55d18
Martins Siksnis <msiksnis@gmail.com>
2024-09-14 18:32:37 +0200

    starting on cart

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Cart from "../components/cart";
 
 export const Route = createFileRoute("/cart")({
-  component: () => <div>Hello /cart!</div>,
+  component: Cart,
 });
