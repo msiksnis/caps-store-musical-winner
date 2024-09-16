393dd3c9ab5913a6a50854f781e0898494bdd3c2
Martins Siksnis <msiksnis@gmail.com>
2024-09-16 18:46:46 +0200

    updated cartStore to be in sync with local storage

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -13,29 +13,65 @@ interface CartState {
   getTotalPrice: () => number;
 }
 
+// Loads the cart from localStorage
+const loadCartFromLocalStorage = (): CartItem[] => {
+  try {
+    const cartItems = localStorage.getItem("cart");
+    return cartItems ? JSON.parse(cartItems) : [];
+  } catch (error) {
+    console.error("Error loading cart from localStorage", error);
+    return [];
+  }
+};
+
+// Saves the cart to localStorage
+const saveCartToLocalStorage = (cartItems: CartItem[]) => {
+  try {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  } catch (error) {
+    console.error("Error saving cart to localStorage", error);
+  }
+};
+
 export const useCartStore = create<CartState>((set, get) => ({
-  cartItems: [],
+  cartItems: loadCartFromLocalStorage(),
+
   addToCart: (product) => {
     const cartItems = get().cartItems;
     const existingItem = cartItems.find((item) => item.id === product.id);
+
+    let newCartItems;
     if (existingItem) {
-      set({
-        cartItems: cartItems.map((item) =>
-          item.id === product.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item,
-        ),
-      });
+      // Increase quantity if item already exists
+      newCartItems = cartItems.map((item) =>
+        item.id === product.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item,
+      );
     } else {
-      set({ cartItems: [...cartItems, { ...product, quantity: 1 }] });
+      // Add new item to cart
+      newCartItems = [...cartItems, { ...product, quantity: 1 }];
     }
+    set({ cartItems: newCartItems });
+    saveCartToLocalStorage(newCartItems);
   },
+
+  // Removes an item from the cart by filtering out the item with the given productId
   removeFromCart: (productId) => {
-    set({
-      cartItems: get().cartItems.filter((item) => item.id !== productId),
-    });
+    const newCartItems = get().cartItems.filter(
+      (item) => item.id !== productId,
+    );
+    set({ cartItems: newCartItems });
+    saveCartToLocalStorage(newCartItems);
   },
-  clearCart: () => set({ cartItems: [] }),
+
+  // Clears the cart by setting cartItems to an empty array
+  clearCart: () => {
+    set({ cartItems: [] });
+    saveCartToLocalStorage([]);
+  },
+
+  // Returns the total price of all items in the cart
   getTotalPrice: () => {
     return get().cartItems.reduce(
       (total, item) => total + item.discountedPrice * item.quantity,
