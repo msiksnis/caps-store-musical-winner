eb426a93b3baf7ffe6e1e90ffc67afcc624e8300
Martins Siksnis <msiksnis@gmail.com>
2024-09-17 20:11:34 +0200

    removed email template

--- netlify/functions/send-email.cjs
+++ netlify/functions/send-email.cjs
@@ -1,5 +1,4 @@
 const { Resend } = require("resend");
-const { ContactMeTemplate } = require("./email-template.cjs");
 
 // Initialize Resend with your API key
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -7,14 +6,39 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 exports.handler = async (event) => {
   try {
     const body = JSON.parse(event.body);
-    const { fullName } = body;
+    const { fullName, subject, email, message } = body;
+
+    // Construct the plain HTML for the email
+    const htmlContent = `
+      <html>
+        <head>
+          <style>
+            body { font-family: Arial, sans-serif; }
+            .container { padding: 20px; }
+            .heading { font-size: 24px; font-weight: bold; }
+            .paragraph { font-size: 18px; margin-top: 20px; }
+            .message { background-color: #f2f3f3; padding: 15px; border-radius: 4px; }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1 class="heading">Subject: ${subject}</h1>
+            <div class="message">
+              <p class="paragraph">${message}</p>
+            </div>
+            <p class="paragraph">Sent by: ${fullName}</p>
+            <p class="paragraph">Email: ${email}</p>
+          </div>
+        </body>
+      </html>
+    `;
 
     // Send email using Resend API
     const data = await resend.emails.send({
       from: `${fullName} <email@mail.devmarty.com>`,
-      to: ["msiksnis@gmail.com"],
+      to: ["msiksnis@gmail.com"], // Recipients email
       subject: "A message through Cap's Store",
-      react: ContactMeTemplate(body),
+      html: htmlContent, // Send plain HTML
     });
 
     return {
